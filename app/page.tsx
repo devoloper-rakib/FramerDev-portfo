7e04429a87abc80997bde9a823b73e8fd1a688e5
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-05-20 21:49:26 +0600

    testimonials section added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
+import Clients from "@/components/Clients";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
